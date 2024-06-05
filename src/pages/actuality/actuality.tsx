6e8427498e53555f3e0fe2e79025fcf53c90b01d
Hugoy <hugopont18@gmail.com>
2024-06-05 20:37:24 +0200

    [hpx][feats][AddArticleOfActuality]

--- src/pages/actuality/actuality.tsx
+++ src/pages/actuality/actuality.tsx
@@ -1,41 +1,126 @@
 function actuality() {
     return (
-        <div className="bg-white py-24 sm:py-32">
+        <div className="bg-white py-8 sm:py-12">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl text-center">
-              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">From the blog</h2>
-              <p className="mt-2 text-lg leading-8 text-gray-600 text-center">Learn how to grow your business with our expert advice.</p>
+              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">Notre actualité</h2>
+              <p className="mt-2 text-lg leading-8 text-gray-600 text-center">Découvrez l'actualité de votre musée virtuel, une actualité disponible pour tous et avec transparence.</p>
             </div>
-            <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            <div
+                className="mx-auto mt-16 grid grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
               <article className="flex flex-col items-start justify-between">
                 <div className="relative w-full">
-                  <img src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80" alt="" className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"/>
+                  <img
+                      src="https://cdn.sanity.io/images/cctd4ker/production/b8a5e07c166342e1c7f6fe30b8c45d64fea69ea5-4096x2326.jpg?w=3840&q=75&fit=clip&auto=format"
+                      alt=""
+                      className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"/>
                   <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
                 </div>
                 <div className="max-w-xl">
                   <div className="mt-8 flex items-center gap-x-4 text-xs">
-                    <time className="text-gray-500">Mar 16, 2020</time>
-                    <a href="#" className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Marketing</a>
+                    <time className="text-gray-500">Mardi 5 juin 2024</time>
+                    <a className="relative cursor-none z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Digital</a>
                   </div>
                   <div className="group relative">
                     <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                      <a href="#">
+                      <a className="cursor-none">
                         <span className="absolute inset-0"></span>
-                        Boost your conversion rate
+                        Importance du musée virtuel
                       </a>
                     </h3>
-                    <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
+                    <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">Illo sint voluptas. Error
+                      voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde.
+                      Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
                   </div>
                   <div className="relative mt-8 flex items-center gap-x-4">
-                    <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" className="h-10 w-10 rounded-full bg-gray-100"/>
+                    <img src="https://www.apollo-magazine.com/wp-content/uploads/2022/08/web-lead_2022-2.jpg?w=768"
+                         alt="" className="h-10 w-10 rounded-full bg-gray-100 object-cover"/>
                     <div className="text-sm leading-6">
                       <p className="font-semibold text-gray-900">
-                        <a href="#">
+                        <a href="https://fr.wikipedia.org/wiki/Max_Hollein" target="_blank">
                           <span className="absolute inset-0"></span>
-                          Michael Foster
+                          Max Hollein
                         </a>
                       </p>
-                      <p className="text-gray-600">Co-Founder / CTO</p>
+                      <p className="text-gray-600">CEO de The Met</p>
+                    </div>
+                  </div>
+                </div>
+              </article>
+              <article className="flex flex-col items-start justify-between">
+                <div className="relative w-full">
+                  <img
+                      src="https://cdn.sanity.io/images/cctd4ker/production/b8a5e07c166342e1c7f6fe30b8c45d64fea69ea5-4096x2326.jpg?w=3840&q=75&fit=clip&auto=format"
+                      alt=""
+                      className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"/>
+                  <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
+                </div>
+                <div className="max-w-xl">
+                  <div className="mt-8 flex items-center gap-x-4 text-xs">
+                    <time className="text-gray-500">Mardi 5 juin 2024</time>
+                    <a className="relative cursor-none z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Digital</a>
+                  </div>
+                  <div className="group relative">
+                    <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                      <a className="cursor-none">
+                        <span className="absolute inset-0"></span>
+                        Importance du musée virtuel
+                      </a>
+                    </h3>
+                    <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">Illo sint voluptas. Error
+                      voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde.
+                      Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
+                  </div>
+                  <div className="relative mt-8 flex items-center gap-x-4">
+                    <img src="https://www.apollo-magazine.com/wp-content/uploads/2022/08/web-lead_2022-2.jpg?w=768"
+                         alt="" className="h-10 w-10 rounded-full bg-gray-100 object-cover"/>
+                    <div className="text-sm leading-6">
+                      <p className="font-semibold text-gray-900">
+                        <a href="https://fr.wikipedia.org/wiki/Max_Hollein" target="_blank">
+                          <span className="absolute inset-0"></span>
+                          Max Hollein
+                        </a>
+                      </p>
+                      <p className="text-gray-600">CEO de The Met</p>
+                    </div>
+                  </div>
+                </div>
+              </article>
+              <article className="flex flex-col items-start justify-between">
+                <div className="relative w-full">
+                  <img
+                      src="https://cdn.sanity.io/images/cctd4ker/production/b8a5e07c166342e1c7f6fe30b8c45d64fea69ea5-4096x2326.jpg?w=3840&q=75&fit=clip&auto=format"
+                      alt=""
+                      className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"/>
+                  <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
+                </div>
+                <div className="max-w-xl">
+                  <div className="mt-8 flex items-center gap-x-4 text-xs">
+                    <time className="text-gray-500">Mardi 5 juin 2024</time>
+                    <a className="relative cursor-none z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Digital</a>
+                  </div>
+                  <div className="group relative">
+                    <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                      <a className="cursor-none">
+                        <span className="absolute inset-0"></span>
+                        Importance du musée virtuel
+                      </a>
+                    </h3>
+                    <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">Illo sint voluptas. Error
+                      voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde.
+                      Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
+                  </div>
+                  <div className="relative mt-8 flex items-center gap-x-4">
+                    <img src="https://www.apollo-magazine.com/wp-content/uploads/2022/08/web-lead_2022-2.jpg?w=768"
+                         alt="" className="h-10 w-10 rounded-full bg-gray-100 object-cover"/>
+                    <div className="text-sm leading-6">
+                      <p className="font-semibold text-gray-900">
+                        <a href="https://fr.wikipedia.org/wiki/Max_Hollein" target="_blank">
+                          <span className="absolute inset-0"></span>
+                          Max Hollein
+                        </a>
+                      </p>
+                      <p className="text-gray-600">CEO de The Met</p>
                     </div>
                   </div>
                 </div>
